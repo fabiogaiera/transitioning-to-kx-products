1c66ab0909b8983f27d90f7ccf16a97b02421f17
Fabio Gaiera <fabio.gaiera@outlook.com>
2025-05-29 21:12:38 +0200

    Added more stuff

--- taq_analysis/taq_chart_creator.py
+++ taq_analysis/taq_chart_creator.py
@@ -1,34 +1,12 @@
 import sys
 
 import matplotlib.pyplot as plt
-import pykx as kx
 
-from benckmark_util import log_execution_time
-
-"""
-CSV format example
-datetime,sym,price,size
-2025.05.05D08:00:00.009039359,IBM,244.56,10
-2025.05.05D08:00:00.156501572,IBM,243,8
-2025.05.05D08:00:00.156579644,IBM,244.03,6
-"""
-
-
-@log_execution_time
-def retrieve_taq_dataframe(csv_file_path_1, csv_file_path_2):
-    # Using PyKX interface
-    trades = kx.q.read.csv(csv_file_path_1, 'PSFJ')
-    quotes = kx.q.read.csv(csv_file_path_2, 'PSFJFJ')
-    # Key the table
-    quotes = kx.q.xkey(['sym', 'datetime'], quotes)
-    # As-Of Join
-    taq_table = kx.q.aj(kx.SymbolVector(['sym', 'datetime']), trades, quotes)
-    # Convert to pandas DataFrame
-    return taq_table.pd()
+from taq_dataframe_creator import retrieve_taq_dataframe_pykx
+from taq_dataframe_creator import retrieve_taq_dataframe_q
 
 
 def create_taq_chart(df):
-
     df.set_index('datetime', inplace=True)
 
     fig = plt.figure()
@@ -53,5 +31,6 @@ if __name__ == "__main__":
         print("Usage: python tag_chart_creator.py </path/to/file/trades.csv> </path/to/file/quotes.csv>")
         sys.exit(1)
 
-    data_frame = retrieve_taq_dataframe(sys.argv[1], sys.argv[2])
+    data_frame = retrieve_taq_dataframe_pykx(sys.argv[1], sys.argv[2])
+    # data_frame = retrieve_taq_dataframe_q(sys.argv[1], sys.argv[2])
     create_taq_chart(data_frame)
