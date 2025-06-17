516c1feb11fe70121281b4d231a97ede70ad8cfb
Fabio Gaiera <fabio.gaiera@outlook.com>
2025-06-17 23:26:19 +0300

    Added stuff

---
+++ intraday_trading_volume_histogram/volume_dataframe_creator.py
@@ -0,0 +1,23 @@
+import pykx as kx
+
+"""
+CSV format example
+datetime,sym,price,size
+2025.05.05D08:00:00.009039359,IBM,244.56,10
+2025.05.05D08:00:00.156501572,IBM,243,8
+2025.05.05D08:00:00.156579644,IBM,244.03,6
+"""
+
+
+def create_dataframe(csv_file_path):
+    # Upload a CSV file into a kdb+ table
+    trades = kx.q.read.csv(csv_file_path, 'PSFJ')
+
+    # Execute a qSQL query using xbar to bucket the minutes into hours
+    trades_table = trades.select(kx.Column('i').count(), by=kx.Column('datetime').minute.xbar(60))
+
+    # Seamless integration with existent Python code (pandas and Matplotlib libraries)
+
+    # Transform to a pandas.DataFrame instance
+    df = trades_table.pd()
+    return df
