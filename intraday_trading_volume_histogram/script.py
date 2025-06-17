ec455a2d32fe7b4ca499d8ac12a790f1f1f24815
Fabio Gaiera <fabio.gaiera@outlook.com>
2025-06-17 23:55:48 +0300

    Renamed scripts

--- intraday_trading_volume_histogram/script.py
+++ intraday_trading_volume_histogram/script.py
@@ -1,12 +1,12 @@
 import sys
 
-from histogram_creator import create_histogram
-from volume_dataframe_creator import create_dataframe
+from trades_dataframe_creator import create_dataframe
+from volumes_histogram_creator import create_histogram
 
 if __name__ == "__main__":
 
     if len(sys.argv) != 2:
         print("Usage: python script.py </path/to/file/trades.csv>")
         sys.exit(1)
-    data_frame = create_dataframe(sys.argv[1])
-    create_histogram(data_frame)
+    trades_data_frame = create_dataframe(sys.argv[1])
+    create_histogram(trades_data_frame)
