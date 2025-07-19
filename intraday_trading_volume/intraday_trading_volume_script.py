1d44dbf27aec11fac32b3a3ca073735b42f8c6ce
Fabio Gaiera <fabio.gaiera@outlook.com>
2025-07-19 15:50:05 +0200

    Fixed some stuff

--- intraday_trading_volume/intraday_trading_volume_script.py
+++ intraday_trading_volume/intraday_trading_volume_script.py
@@ -5,16 +5,21 @@ from intraday_trading_volume.volumes_histogram_creator import create_histogram
 
 """
 Usage in Linux / Mac:
-python -m intraday_trading_volume.intraday_trading_volume_script /path/to/file/trades.csv
+python -m intraday_trading_volume.intraday_trading_volume_script /path/to/file/trades.csv 2025.06.06
 
 Usage in Windows: 
-python -m intraday_trading_volume.intraday_trading_volume_script C:/path/to/file/trades.csv
+python -m intraday_trading_volume.intraday_trading_volume_script C:/path/to/file/trades.csv 2025.06.06
 """
 
 if __name__ == "__main__":
 
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
         print("Incorrect parameters")
         sys.exit(1)
-    trades_data_frame = create_dataframe(sys.argv[1])
+
+    # Daylight Saving Time (DST) for Eastern Time (ET) in the U.S. as we're analyzing the IBM ticker.
+    market_open = '2025.06.06D13:30:00.000000000'
+    market_close = '2025.06.06D20:00:00.000000000'
+
+    trades_data_frame = create_dataframe(sys.argv[1], sys.argv[2], market_open, market_close)
     create_histogram(trades_data_frame)
--- intraday_trading_volume/python/trades_dataframe_creator.py
+++ intraday_trading_volume/python/trades_dataframe_creator.py
@@ -12,13 +12,28 @@ datetime,sym,price,size
 """
 
 
-def create_dataframe(csv_file_path):
+def create_dataframe(csv_file_path, date, market_open, market_close):
     # Upload a CSV file into a kdb+ table
     trades = kx.q.read.csv(csv_file_path, 'PSFJ')
 
+    # Filter trades data by day
+    intraday_trades = trades.select(
+        where=(
+                (kx.Column('datetime').cast('date')) == kx.q(date)
+        )
+    )
+
+    # Filter trades data considering only market hours
+    filtered_intraday_table = intraday_trades.select(
+        where=(
+                (kx.Column('datetime') >= kx.q(market_open)) &
+                (kx.Column('datetime') <= kx.q(market_close))
+        )
+    )
+
     # Execute a qSQL query using xbar to bucket the minutes into hours
-    trades_table = trades.select(kx.Column('trade_count', value=kx.Column('i').count()),
-                                 by=kx.Column('time', value=kx.Column('datetime').minute.xbar(60)))
+    trades_table = filtered_intraday_table.select(kx.Column('trade_count', value=kx.Column('i').count()),
+                                                  by=kx.Column('time', value=kx.Column('datetime').minute.xbar(60)))
 
     # Transform to a pandas.DataFrame instance
     return trades_table.pd()
