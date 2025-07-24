d7a2e3abad19a45c6f34af92c64c28c9c37f6f2c
Fabio Gaiera <fabio.gaiera@outlook.com>
2025-07-24 21:08:26 +0200

    Added fixes

--- intraday_trading_volume/python/trades_dataframe_creator.py
+++ intraday_trading_volume/python/trades_dataframe_creator.py
@@ -18,27 +18,31 @@ def create_dataframe(csv_file_path, date, market_open, market_close):
     # S stands for symbol
     # F stands for float
     # J stands for long
-    trades = kx.q.read.csv(csv_file_path, 'PSFJ')
+    trades = kx.q.read.csv(csv_file_path, [kx.TimestampAtom, kx.SymbolAtom, kx.FloatAtom, kx.LongAtom])
 
     # Filter trades data by day
     # Our datetime field is timestamp data type, but we have to cast it to date for filtering
     intraday_trades = trades.select(
         where=(
-                (kx.Column('datetime').cast('date')) == kx.q(date)
+                (kx.Column('timestamp').cast('date')) == date
         )
     )
 
     # Filter trades data considering only market hours
     filtered_intraday_trades = intraday_trades.select(
         where=(
-                (kx.Column('datetime') >= kx.q(market_open)) &
-                (kx.Column('datetime') <= kx.q(market_close))
+                (kx.Column('timestamp') >= market_open) &
+                (kx.Column('timestamp') <= market_close)
         )
     )
 
     # Execute a qSQL query using xbar to bucket the minutes into hours
-    trades_table = filtered_intraday_trades.select(kx.Column('trade_count', value=kx.Column('i').count()),
-                                                   by=kx.Column('time', value=kx.Column('datetime').minute.xbar(60)))
+    trades_table = filtered_intraday_trades.select(kx.Column('trade_sum', value=kx.Column('i').sum()),
+                                                   by=kx.Column('time', value=kx.Column('timestamp').minute.xbar(60)))
+
+    # Other way to rename columns
+    trades_table = filtered_intraday_trades.select(kx.Column('i').sum().name('trade_sum'),
+                                                   by=kx.Column('timestamp').minute.xbar(60))
 
     # Transform to a pandas.DataFrame instance
     return trades_table.pd()
