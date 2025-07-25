9a24e60a3ba9bd307da784f3f9556b9cfdb89e0b
Fabio Gaiera <fabio.gaiera@outlook.com>
2025-07-25 20:59:13 +0200

    Modified variables

--- intraday_trading_volume/q/trades_dataframe_creator.py
+++ intraday_trading_volume/q/trades_dataframe_creator.py
@@ -5,7 +5,7 @@ import pykx as kx
 
 """
 CSV format example
-datetime,sym,price,size
+timestamp,sym,price,size
 2025.05.05D08:00:00.009039359,IBM,244.56,10
 2025.05.05D08:00:00.156501572,IBM,243,8
 2025.05.05D08:00:00.156579644,IBM,244.03,6
@@ -22,13 +22,13 @@ def create_dataframe(csv_file_path, date, market_open, market_close):
 
     # Filter trades data by day
     # Our datetime field is timestamp data type, but we have to cast it to date for filtering
-    kx.q(f'intraday_trades: select from trades where (`date$datetime) = {date}')
+    kx.q(f'intradayTrades: select from trades where (`date$timestamp) = {date}')
 
     # Filter trades data considering only market hours
-    kx.q(f'filtered_intraday_trades: select from intraday_trades where datetime within {market_open} {market_close}')
+    kx.q(f'filteredIntradayTrades: select from intradayTrades where timestamp within {market_open} {market_close}')
 
     # Execute a qSQL query using xbar to bucket the minutes into hours
-    trades_table = kx.q('select trade_count:count i by time:60 xbar datetime.minute from filtered_intraday_trades')
+    trades_table = kx.q('select tradeCount: count i by time: 60 xbar timestamp.minute from filteredIntradayTrades')
 
     # Transform to a pandas.DataFrame instance
     return trades_table.pd()
