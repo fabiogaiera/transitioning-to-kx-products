c16e26e314c0e43fb18056f520137bb8f1cce23e
Fabio <fabio.gaiera@outlook.com>
2025-08-04 20:15:46 +0200

    Minor fixes

--- bid_ask_spread/bid_ask_spread_script.py
+++ bid_ask_spread/bid_ask_spread_script.py
@@ -3,14 +3,14 @@
 import sys
 
 from bid_ask_spread.bid_ask_spread_density_plot_creator import create_density_plot
-from bid_ask_spread.q.bid_ask_spread_df_creator import retrieve_bid_ask_spread_df
+from bid_ask_spread.python.bid_ask_spread_df_creator import retrieve_bid_ask_spread_df
 
 """
 Usage in Linux / Mac:
-python -m bid_ask_spread.bid_ask_spread_script /path/to/file/trades.csv /path/to/file/quotes.csv"
+python -m bid_ask_spread.bid_ask_spread_script /path/to/file/trades.csv /path/to/file/quotes.csv
 
 Usage in Windows: 
-python -m bid_ask_spread.bid_ask_spread_script C:/path/to/file/trades.csv C:/path/to/file/quotes.csv"
+python -m bid_ask_spread.bid_ask_spread_script C:/path/to/file/trades.csv C:/path/to/file/quotes.csv
 """
 
 if __name__ == "__main__":
