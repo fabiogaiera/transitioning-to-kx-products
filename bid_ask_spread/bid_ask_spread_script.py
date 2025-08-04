1e5d8d784da3da7445710a18bfbcd50bad5824d3
7e0240a4-b849-4250-80fc-180cafa9a2dd <fabio.gaiera@juliusbaer.com>
2025-08-04 16:36:44 +0200

    Fixed dates

--- bid_ask_spread/bid_ask_spread_script.py
+++ bid_ask_spread/bid_ask_spread_script.py
@@ -20,8 +20,8 @@ if __name__ == "__main__":
         sys.exit(1)
 
     # Daylight Saving Time (DST) for Eastern Time (ET) in the U.S. as we're analyzing the IBM ticker.
-    market_open = '2025.05.23D13:30:00.000000000'
-    market_close = '2025.05.23D20:00:00.000000000'
+    market_open = '2025.06.16D13:30:00.000000000'
+    market_close = '2025.06.16D20:00:00.000000000'
 
     bid_ask_spread_df = retrieve_bid_ask_spread_df(sys.argv[1], sys.argv[2], market_open, market_close)
     create_density_plot(bid_ask_spread_df)
